a0c11095c3068023a15f7a419d67eba37d49abb3
lemuellloren <lempakz@yahoo.com>
2022-01-25 10:00:11 +0800

    add signClickHandler Function

--- src/App.js
+++ src/App.js
@@ -50,6 +50,19 @@ const App = () => {
     });
   };
 
+  // * signClickHandler Function 
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+
   return (
     <Wrapper>
       <Screen value={calc.num ? calc.num : calc.res} />
