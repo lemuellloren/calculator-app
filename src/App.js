c4b1de9abd01470615dc0e112deca1dc0bdde6c0
lemuellloren <lempakz@yahoo.com>
2022-01-25 09:14:38 +0800

    remove Screen component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Wrapper from "./components/Wrapper";
-import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
 
 function App() {
   return (
     <Wrapper>
-      <Screen value="0" />
       <ButtonBox>
         <Button
           className=""
